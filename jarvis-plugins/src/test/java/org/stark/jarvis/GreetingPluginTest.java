2506e58463abf147ebb607ee8b7722f1eee53782
fuyongde <fuyongde@foxmail.com>
2020-12-15 19:17:44 +0800

    :bug: 修复在新的机器上无法安装自定义插件的问题

--- jarvis-plugins/src/test/java/org/stark/jarvis/GreetingPluginTest.java
+++ jarvis-plugins/src/test/java/org/stark/jarvis/GreetingPluginTest.java
@@ -6,15 +6,17 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * A simple unit test for the 'org.stark.jarvis.plugins.greeting' plugin.
+ * A simple unit test for the 'org.stark.jarvis.greeting' plugin.
  */
 public class GreetingPluginTest {
 
     @Test
     public void pluginRegistersATask() {
+        // Create a test project and apply the plugin
         Project project = ProjectBuilder.builder().build();
         project.getPlugins().apply("org.stark.jarvis.plugins.greeting");
 
+        // Verify the result
         assertNotNull(project.getTasks().findByName("hello"));
     }
 }
